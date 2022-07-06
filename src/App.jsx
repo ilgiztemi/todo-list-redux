a3f845421194768bf20e2ba31ffd72e9edf39eb5
Ilgiz Temi <98427716+ilgiztemi@users.noreply.github.com>
2022-07-05 23:56:14 -0400

    small fixes

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import "./styles.css";
 import { useDispatch, useSelector } from "react-redux";
 import * as actions from "./actions";
